00195f4ab8296925d88ab294c2120ef24292467f
Sriram kanakam <92006184+Sri-k23@users.noreply.github.com>
2023-10-08 03:44:31 +0530

    All major files updated

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+//components
+import ParentComponents from './ParentComponents';
+
+function App() {
+  return (
+    <div className="App">
+      <ParentComponents></ParentComponents>
+    </div>
+  );
+}
+
+export default App;
